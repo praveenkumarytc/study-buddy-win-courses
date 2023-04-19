854d3f0d5b56e491091046ef7e6e35c6ee90fbaf
praveenkumarytc <praveenkumarytc@gmail.com>
2023-04-19 12:12:12 +0530

    updated code

--- lib/images.dart
+++ lib/images.dart
@@ -3,4 +3,12 @@ class Images {
   static const String lockedCard = 'assets/images/security.png';
   static const String unlockedCard = 'assets/images/success.png';
   static const String googleLoading = 'assets/images/google-icon-_1__1.gif';
+  //   static const String appLogo = 'assets/images/study_buddy_logo.png';
+  // static const String lockedCard = 'assets/images/security.png';
+  // static const String unlockedCard = 'assets/images/success.png';
+  // static const String googleLoading = 'assets/images/google-icon-_1__1.gif';
+  //   static const String appLogo = 'assets/images/study_buddy_logo.png';
+  // static const String lockedCard = 'assets/images/security.png';
+  // static const String unlockedCard = 'assets/images/success.png';
+  // static const String googleLoading = 'assets/images/google-icon-_1__1.gif';
 }
